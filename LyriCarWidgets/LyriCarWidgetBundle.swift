b5eac91aaa5619376803a5f404b4e8d9bf7b2755
Marlius92 <61624786+Marlius92@users.noreply.github.com>
2026-09-13 02:07:04 +0200

    Add LyriCar multi-widget CarPlay support

--- LyriCarWidgets/LyriCarWidgetBundle.swift
+++ LyriCarWidgets/LyriCarWidgetBundle.swift
@@ -5,5 +5,8 @@ import WidgetKit
 struct LyriCarWidgetBundle: WidgetBundle {
     var body: some Widget {
         LyriCarLiveActivity()
+        LyriCarBeforeWidget()
+        LyriCarCurrentWidget()
+        LyriCarAfterWidget()
     }
 }
